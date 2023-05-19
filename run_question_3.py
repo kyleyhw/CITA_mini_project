f2116c82f484358851bcffcec0cafe0ef6be6bd3
Kyle <kyleyhw@gmail.com>
2023-05-19 00:21:10 -0400

    outputs the random walks as plots converging to the same point

--- run_question_3.py
+++ run_question_3.py
@@ -15,18 +15,18 @@ data = Data(filepath + filename)
 
 ndim = 2
 nwalkers = 32
-p0 = np.random.uniform(size=(nwalkers, ndim), low=(1e-8, 1e-7), high=(1e-4, 1e-3))
+p0 = np.random.uniform(size=(nwalkers, ndim), low=(1e-7, 1e-5), high=(1e-5, 1e-3))
 
 sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, args=[data.times, data.signal])
 
 sampler.run_mcmc(p0, 10)
 
-samples = sampler.get_chain(flat=True)
+samples = sampler.get_chain()
 print(samples.shape)
 print(samples[0])
 
-epsilon_walk = samples.T[0]
-df_dt_walk = samples.T[1]
+epsilon_walk = samples[:, :, 0]
+df_dt_walk = samples[:, :, 1]
 
 plt.figure()
 plt.plot(epsilon_walk, df_dt_walk)
